48f668aa5801629df896f89f3d048b792a4f9c0d
zhangqiao <zhangqiao@hbbeisheng.com>
2020-11-14 17:27:02 +0800

    页面初加载时调用provider里的方法

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -7,6 +7,7 @@ import 'package:massageflutterapp/utils/size_fit.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:provider/provider.dart';
 import 'package:massageflutterapp/view_model/home_model.dart';
+import 'package:massageflutterapp/provider/provider_widget.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -15,13 +16,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _searchController=TextEditingController(text: '');
   @override
+  void initState() {
+    print('object');
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(S.of(context).homeTitle),
       ),
-      body: Consumer<HomeModel>(
+      body: ProviderWidget<HomeModel>(
+        onModelReady: (homeModel){
+          //初始化状态是调用页面初始数据
+          homeModel.loadHome();
+        },
         builder: (context,homeModel,child){
           return NotificationListener(  //页面有输入框时的失焦操作
               onNotification: (ScrollNotification notification) {
@@ -180,6 +191,7 @@ class _HomePageState extends State<HomePage> {
               )
           );
         },
+        model: HomeModel(),
       )
     );
   }
--- lib/view_model/home_model.dart
+++ lib/view_model/home_model.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/cupertino.dart';
 
 class HomeModel extends ChangeNotifier{
+   //  mock
    List _homeType=['home_type1.png','home_type2.png','home_type3.png','home_type4.png'];
    List _banners = ['home_banner.png'];
 
    List get homeType=>_homeType;
    List get banners=>_banners;
 
+   Future<bool> loadHome() async {
+     try {
+       print('load home');
+       return true;
+     } catch (e, s) {
 
+       return false;
+     }
+   }
 
 }
